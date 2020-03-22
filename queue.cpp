2012e9a44fdfb27a3c2c88a399ec8920ea011761
Piotr <swarog.piotr@gmail.com>
2020-03-22 16:09:27 +0100

    Created public interface

--- queue.cpp
+++ queue.cpp
@@ -1,5 +1,40 @@
 #include "queue.h"
 
-Queue::Queue()
+
+Queue::Queue(int size) : queue_size(size)
 {
+
+}
+
+Queue::~Queue()
+{
+}
+
+void Queue::remove()
+{
+}
+
+bool Queue::isEmpty() const
+{
+	return false;
+}
+
+bool Queue::isFull() const
+{
+	return false;
+}
+
+int Queue::size() const
+{
+	return 0;
+}
+
+bool Queue::enqueue(const Item& item)
+{
+	return false;
+}
+
+bool Queue::dequeue(Item& item)
+{
+	return false;
 }
---
+++ queue.h
@@ -0,0 +1,45 @@
+#pragma once
+
+typedef long Item;
+
+class Queue
+{
+private:
+	enum {Q_SIZE = 10};
+    const int queue_size;
+
+
+
+public:
+	Queue(int size = Q_SIZE);
+	~Queue();
+	
+	bool enqueue(const Item& item);
+	bool dequeue(Item& item);
+
+	void remove();
+	bool isEmpty() const;
+	bool isFull() const;
+	int size() const;
+};
+
+		
+//public interface:
+
+/*
+- check if empty
+- check if full
+- check how many items
+
+- create empty queue(with item limit)
+- delete queue
+
+- add item (push?) to the end of queue /enqueue
+- pull item from the head of queue /dequeue
+
+
+
+
+
+
+*/
